a9a1d6db7ba1249011cdaa5882222d2adec2e15a
Gergely Garai <garai.gergely22@gmail.com>
2024-05-02 20:09:59 +0000

    add own content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'http://husi.lol',
+  integrations: [mdx(), sitemap(), tailwind()],
+  base: '/asd'
 });
